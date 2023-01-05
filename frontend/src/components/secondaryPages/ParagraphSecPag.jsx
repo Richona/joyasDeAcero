b509003e8b2477f8803150507ac884991f677fac
Richona <105828822+Richona@users.noreply.github.com>
2023-01-05 20:02:17 -0300

    small detail fixed to howToBuy

--- frontend/src/components/secondaryPages/ParagraphSecPag.jsx
+++ frontend/src/components/secondaryPages/ParagraphSecPag.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./ParagraphSecPag.css"
 
+import { Link } from 'react-router-dom';
+
 export default function ParagraphSecPag(props) {
   return (
     <p className='paragraphSecPag'>
         {props.paragraph}
         {props.b && <b>{props.b}</b>}
-        {props.link && <b><a href={props.link}>{props.a}</a></b>}
+        {props.link && <b><Link to={props.link}>{props.a}</Link></b>}
         {props.small && <small className='paragraphSecPag'><br />{props.small}</small>}
     </p>
   )
