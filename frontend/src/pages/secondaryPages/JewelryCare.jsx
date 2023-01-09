c7f1ebb03f03026c9bb3eac29b01b504413b500c
Richona <105828822+Richona@users.noreply.github.com>
2023-01-09 15:49:05 -0300

    modify JewelryCare

--- frontend/src/pages/secondaryPages/JewelryCare.jsx
+++ frontend/src/pages/secondaryPages/JewelryCare.jsx
@@ -1,12 +1,57 @@
 import React from 'react'
 import "./SecondPages.css"
 
+import HomeSectionSecPag from '../../components/secondaryPages/HomeSectionSecPag';
+import TitleH2SecPag from '../../components/secondaryPages/TitleH2SecPag';
+import ParagraphSecPag from '../../components/secondaryPages/ParagraphSecPag';
+
 export default function JewelryCare() {
     return (
         <div className='SecondPage_container'>
             <section className='SecondPage_container_section1'>
-                Cuidado de las joyas
-
+                <HomeSectionSecPag title="Cuidado de las joyas" />
+                <article className='SecondPage_container_article1'>
+                    <TitleH2SecPag title="¿NECESITAS SABER CÓMO CUIDAR TUS JOYAS?" />
+                    <ParagraphSecPag paragraph="Con estos sencillos consejos vas a aprender a cuidar las joyas. Queremos que luzcan siempre bonitas, relucientes y brillantes como el primer día. Seguí nuestras recomendaciones para mantener y conservar los accesorios y que tus clientes disfruten para siempre de sus accesorios."/>
+                    <br /><br />
+                    <ParagraphSecPag b="Cuidado de las Joyas de Plata."/>
+                    <ParagraphSecPag paragraph="La transpiración el perfume y el agua salada son las 3 cosas más dañinas para la plata."/>
+                    <ParagraphSecPag paragraph="No temas si la plata se ennegrece. Si las joyas entran en contacto con algún producto que pueda oscurecerlas, o a veces simplemente el ph de nuestra piel hace que pierdan su brillo, no tengas miedo, la plata puede limpiarse."/>
+                    <br />
+                    <ParagraphSecPag paragraph="Hay varias formas de limpiar la Plata. Lo que más resultado nos da a nosotros es el Platalim. Podes conseguirlo en mercado libre. Una vez que lo tengas, solo necesitas una toalla y frotar tus joyas de plata. Cuando la joya quede brillante, limpiala con papel y saca todo el producto restante. Finalmente, enjuagá la joya con agua y jabón y secala nuevamente. Es aconsejable hacer este método con guantes, ya que el producto es corrosivo por eso aconsejamos usarlo pocas veces en las piezas. Hay que tener cuidado si hay piedras, es mejor que el producto no toque la piedra ya que podría dañarla."/>
+                    <br />
+                    <ParagraphSecPag paragraph="Otro método es con el Bicarbonato, agua caliente, trapo de algodón y papel de aluminio. En este método necesitarás calentar el agua y poner en un bol el papel de aluminio con la cara mate del papel hacia abajo, agregas el agua caliente con dos cucharadas de bicarbonato y finalmente las joyas, solo tendrás que esperar mientras remueves las joyas y luego secarlas bien con un trapo de algodón. Podes repetir el proceso en caso de que no queden bien a la primera. En este método hay que tener cuidado si la joya contiene plata oscurecida, si tu joya tiene relieve y profundidad con una parte “negra” o más oscura, si usas este método desaparecerá esa parte del diseño por completo. Por eso está recomendado para cadenas, pero si tienes alguna joya totalmente plata también podes usarlo."/>
+                    <ParagraphSecPag paragraph="Otras opciones son:"/>
+                    <ParagraphSecPag paragraph="Dentífrico y agua"/>
+                    <ParagraphSecPag paragraph="agua hirviendo pocos segundos y detergente"/>
+                    <ParagraphSecPag paragraph="Paño mágico o toalla suave"/>
+                    <br /><br />
+                    <ParagraphSecPag b="Cuidado de las Joyas de Enchapado en Oro."/>
+                    <ParagraphSecPag paragraph="Muy importante es guardar las joyas en una bolsita dentro de una cajita o joyero."/>
+                    <ParagraphSecPag paragraph="Evita lavarte las manos o ducharte con ellas para evitar que el baño se vaya perdiendo (aplica también en la pileta o en el mar)"/>
+                    <ParagraphSecPag paragraph="No utilizar cremas, perfumes, maquillajes y químicos mientras usemos nuestras joyas."/>
+                    <ParagraphSecPag paragraph="No realizar acitivdades deportivas con las joyas puestas ni dormir con ellas."/>
+                    <ParagraphSecPag paragraph="No debes exponer tus joyas a cambios bruscos de temperatura."/>
+                    <ParagraphSecPag paragraph="¡A tener en cuenta! El propio PH de la piel puede alterar el color de las joyas."/>
+                    <ParagraphSecPag paragraph="Para su correcta limpieza: utilizar sólo un paño de algodón con agua tibia y jabón neutro, dejarlas reposar por unos diez minutos y luego limpiarlas de forma suave. No recomendamos limpiarlas constantemente sino sólo cuando las joyas están perdiendo un poco su brillo."/>
+                    <br /><br />
+                    <ParagraphSecPag b="Cuidado de las Joyas de Acero Blanco"/>
+                    <ParagraphSecPag paragraph="El Acero Blanco posee un baño de otros metales que le da ese brillo característico. Cuanto más lo cuides más duración tendrá dicho color. Evitá mojarlo, no expongas a abrasivos, podes limpiarlo con un paño seco."/>
+                    <br /><br />
+                    <ParagraphSecPag b="Cuidado de las Joyas de Acero Dorado"/>
+                    <ParagraphSecPag paragraph="Las joyas en acero dorado son joyas de acero con baño dorado. Por tratarse de un baño, el color dorado se puede ir yendo con el uso. Evita que joyas de acero dorado tengan contacto con líquidos como perfumes, cremas, agua oxigenada, productos de limpieza, agua con cloro, alcohol en gel entre otros para conservar su buen estado. Si se mojan, asegúrate de secarlos para evitar que se manchen."/>
+                    <br /><br />
+                    <ParagraphSecPag b="Cómo guardar las joyas"/>
+                    <ParagraphSecPag paragraph="Para que la Plata o el acero Blanco y dorado no se oscurezcan y duren más, es importante saber cómo y dónde guardar tus joyas. Sigue nuestros consejos para mantener tus joyas bien guardadas cuando no las estés usando."/>
+                    <ParagraphSecPag paragraph="En primer lugar, te aconsejamos que las pongas donde no les pueda dar el sol o la luz. Y siempre que haya más de una joya es aconsejable guardarlas por separado."/>
+                    <ParagraphSecPag paragraph="Por otro lado, es muy importante que cierres la cadena de tus collares y pulseras antes de guardarlos, así evitarás que se enreden entre ellas. "/>
+                    <ParagraphSecPag paragraph="Y, por último, te aconsejamos que no las tengas en el baño ya que es una zona de mucha humedad y esto puede oscurecer las joyas. Es mejor tenerlas en un lugar seco libre de humedades, por ejemplo, en tu cómoda o mesita de luz."/>
+                    <br /><br />
+                    <ParagraphSecPag b="Paño mágico"/>
+                    <ParagraphSecPag paragraph="Recomendamos comprar el paño mágico para la limpieza de las joyas. Primero se debe Frotar Fuerte la pieza a limpiar con el paño Blanco (ya se encuentra impregnado de pasta abrillantadora). Por último Frotar Suave la pieza con el paño de color lustrador.  "/>
+                    <br /><br />
+                    <ParagraphSecPag paragraph="Ahora que conoces nuestros truquitos para conservar la plata en el mejor estado posible ya podes acceder a nuestros productos con tranquilidad. "/>
+                </article>
             </section>
         </div>
     )
